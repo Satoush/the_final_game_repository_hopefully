9998713f5e69145db629f8f9caa1ef85be0ac50c
Josh <delrosario2809@gmail.com>
2022-03-13 15:07:15 +0000

    animation of player sort of done

--- character_class_v2.py
+++ character_class_v2.py
@@ -2,17 +2,42 @@ import pygame
 from Bullet_v2 import bullet
 import math
 
-screen = pygame.display.set_mode((800, 600))
+# screen = pygame.display.set_mode((800, 600))
 
 class Character():
-    def __init__(self,X,Y,changeX,changeY,targetY,targetX):
-        super().__init__()
-        self.image = pygame.image.load('Assets/example.png')
+    def __init__(self,X,Y,targetY,targetX,colour):
+        self.sprite_sheets = []
+        self.sprite = [] # holds individual sprites
+        self.image = pygame.image.load('Assets/User_icon.png').convert_alpha()
+        self.sprite_sheets.append(pygame.image.load('The_New_Assets/Character/static idle.png').convert_alpha()) #0
+        self.sprite_sheets.append(pygame.image.load('The_New_Assets/Character/move without FX.png').convert_alpha())
+        self.sprite_sheets.append(pygame.image.load('The_New_Assets/Character/shoot without FX.png').convert_alpha())
+        self.sprite_sheets.append(pygame.image.load('The_New_Assets/Character/damaged.png').convert_alpha())
+        self.sprite_sheets.append(pygame.image.load('The_New_Assets/Character/death.png').convert_alpha())
+        self.current_sprite_sheet = 0 # gets the current sprite sheet from the list
+        self.last_update = pygame.time.get_ticks()
+
+        self.animation_stage = 0
+        self.animation_steps = [1,8,4,2,6]
+        self.current_state = 0
+        self.rect = self.image.get_rect(topleft =(400, 300))
+        #self.new_image =
+
+
+
+        self.width = 48
+        self.height = 26
+        self.cooldown = 100
+        self.frame = 0
+        self.step_counter = 0
+        self.scale = 3
+        self.colour = colour
+
         self.X = X
         self.Y = Y
-        self.changeX = changeX
-        self.changeY = changeY
-        self.velocity = 0.5
+        self.changeX = 0
+        self.changeY = 0
+        self.velocity = 0.25
         self.bullets = []
         self.bullet_vel = 10
         self.targetX = targetX
@@ -31,8 +56,12 @@ class Character():
         if key[pygame.K_DOWN] or key[pygame.K_UP] or key[pygame.K_w] or key[pygame.K_s]:
             self.changeY = self.velocity * - (int(key[pygame.K_UP] or key[pygame.K_w]) * 2 - 1)
 
+
+
+
         if key[pygame.K_LEFT] or key[pygame.K_RIGHT] or key[pygame.K_a] or key[pygame.K_d]:
             self.changeX = self.velocity * - (int(key[pygame.K_LEFT] or key[pygame.K_a]) * 2 - 1)
+
 
 
         self.X += self.changeX
@@ -41,20 +70,64 @@ class Character():
         self.rect[0] = self.X
         self.rect[1] = self.Y
 
+    def get_image(self, frame_of_image, width, height, scale,colour):
+        image = pygame.Surface((width,height)).convert_alpha()
+        image.blit(self.sprite_sheets[self.current_state - 1],(0,0), (0, (frame_of_image * height), width, height))
+        image = pygame.transform.scale(image,(width * scale, height * scale))
+        image.set_colorkey(colour)
+
+        return image
+
 
     def draw_character(self):
+        for animation in self.animation_steps:
+            temp_list = []
+            self.current_state += 1
+            for i in range(animation):
+                temp_list.append(self.get_image(self.step_counter, self.width, self.height, 3, self.colour))
+                self.step_counter += 1
+            self.step_counter = 0
+            self.sprite.append(temp_list)
+
+
+    def animate_character(self,screen,colour):
+        #Update, this is where the character get animated
+        current_time = pygame.time.get_ticks()  # takes the time the code is being executed
+        if current_time -  self.last_update  >= self.cooldown:
+            self.frame += 1
+            self.last_update = current_time  # resets the cooldown
+            if self.current_sprite_sheet == 2 or self.current_sprite_sheet == 3 or self.current_sprite_sheet == 4:
+                self.frame = len(self.sprite[self.current_sprite_sheet]) - 1
+            elif self.frame >= len(self.sprite[self.current_sprite_sheet]):  # makes sure the list doesnt go out of range
+                self.frame = 0
 
         mx, my = pygame.mouse.get_pos()
-
         dist_X = mx - self.X
         dist_Y = my - self.Y
 
         angle = math.atan2(dist_X, dist_Y)  * (180 / math.pi)
 
-        rotated_image = pygame.transform.rotate(self.image, angle)
-        #pygame.draw.line(screen, pygame.Color("red"), (self.X, self.Y), (mx, my))
-        screen.blit(rotated_image, (self.X,self.Y))
-        # pygame.draw.rect(screen,"red" , (self.rect))
+
+        if angle <= 0:
+            flipped_image = pygame.transform.flip(self.sprite[self.current_sprite_sheet][self.frame], True, False)
+            flipped_image.set_colorkey(colour)
+            new_image = flipped_image
+        else:
+            image = self.sprite[self.current_sprite_sheet][self.frame]
+            new_image = image
+
+
+
+        # Show frame image
+        screen.blit(new_image,(self.X, self.Y))
+
+
+
+        # self.frame_of_image += 1
+        # rotated_image = pygame.transform.rotate(self.image, angle)
+        # #pygame.draw.line(screen, pygame.Color("red"), (self.X, self.Y), (mx, my))
+        # screen.blit(rotated_image, (self.X,self.Y))
+        #pygame.draw.rect(screen,"red" , (self.rect))
 
     def boundaries(self):
         if self.X <= 0:
@@ -103,4 +176,4 @@ class Character():
     def update(self):
         self.player_input()
         self.boundaries()
-        self.draw_character()
+        #self.draw_character()
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ pygame
 math
 random
 '''
+import time
 
 import pygame
 # from pygame.locals import *
@@ -16,7 +17,7 @@ from Tank_Enemy import tank_enemy
 
 # Intialize pygame
 pygame.init()
-
+mx, my = pygame.mouse.get_pos()
 font = pygame.font.SysFont("font/Pixeltype.ttf",75)
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
@@ -26,35 +27,28 @@ screen = pygame.display.set_mode((800, 600))
 # Players data
 playerX = 400
 playerY = 300
-playerX_change = 0
-playerY_change = 0
 rounds = 0
 kills = 0
 damage_taken = 0
+image_dimensions_x = 48
+image_dimensions_y = 26
+last_update = pygame.time.get_ticks()
+cooldown = 3000 # how fast the animation is
+step_counter = 0
+
 
 # Enemies data
 Enemy_list = []
-
 changeX  = 0
 changeY = 0
 velocity = 0.01
 fast_velocity = 0.05
-
 num_of_enemies = 6
-mx, my = pygame.mouse.get_pos()
+
+
 
 # Player object
-character = Character(playerX,playerY,playerX_change,playerY_change,mx,my)
-
-
-
-# Entity groups
-#print(zombie)
-# for i in range (num_of_enemies):
-#     Enemy_list.append(enemy(rect_lst))
-
-
-# zombie_group = pygame.sprite.Group()#
+character = Character(playerX, playerY, mx, my,BLACK)
 
 def spawn(num_of_enemies):
     count = 0
@@ -81,7 +75,9 @@ def spawn(num_of_enemies):
 
 
 
+character.draw_character()
 
+image = (pygame.image.load('The_New_Assets/Character/static idle.png').convert_alpha())
 # -------- Main Program Loop -----------
 
 def main_menu():
@@ -102,7 +98,7 @@ def main_menu():
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if Play_Button.pressed(mx,my):
-                    game(rounds,kills,num_of_enemies,damage_taken)
+                    game(rounds, kills,num_of_enemies, damage_taken, last_update, cooldown)
                 if Exit_Button.pressed(mx,my):
                     pygame.quit()
                     exit()
@@ -110,31 +106,51 @@ def main_menu():
         pygame.display.update()
 
 
-def game(rounds,kills,num_of_enemies,damage_taken):
+def game(rounds,kills,num_of_enemies, damage_taken, last_update,  cooldown):
     running = True
     while running:
 
         # Background colour
         screen.fill(WHITE)
+        character.animate_character(screen, BLACK)
+
 
         # Adding the exit button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w or event.key == pygame.K_s\
+                        or event.key == pygame.K_a or event.key == pygame.K_d:
+                    character.current_sprite_sheet = 1
+                    character.frame = 0
+            else:
+                if event.type == pygame.KEYUP:
+                    character.current_sprite_sheet = 0
+                    character.frame = 0
 
 
-            # Fire button
+            # if event.type == pygame.K_DOWN or pygame.K_UP:
+            #     character.current_sprite_sheet = 1
+            #     character.frame = 0
+                        # Fire button
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                   character.fire()
+                    character.frame = 0
+                    character.current_sprite_sheet = 2
+                    character.fire()
 
         for e in Enemy_list:
             if character.has_collided(e.rect) == True:
-                running = False
-                e.destroy(e,Enemy_list)
-                spawn(1)
-                print('hit')
+                character.current_sprite_sheet = 4
+                character.frame = 0
+                e.destroy(e, Enemy_list)
+                current_time = pygame.time.get_ticks()  # takes the time the code is being executed
+                if current_time - last_update >= cooldown:
+                    running = False
+                    spawn(1)
                 # EnemyX = random.randint(0, 725)
                 # EnemyY = random.randint(0, 600)
                 # zombie = enemy(EnemyX, EnemyY)
@@ -176,9 +192,8 @@ def game(rounds,kills,num_of_enemies,damage_taken):
 
 
 
-spawn(num_of_enemies)
+spawn(1)
 
 # Calling main program
 main_menu()
 #game(rounds,kills,num_of_enemies)
-
